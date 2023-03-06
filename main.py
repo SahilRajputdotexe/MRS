5d949d194ed10ed0d5e4c622c4d11392da5b3873
Sahil Rajput <ajstylespheno@gmail.com>
2023-03-06 10:17:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,23 @@ warnings.filterwarnings("ignore")
 
 spotify_df = pd.read_csv('D:/cpp/MRS/dataset.csv')
 
-print(spotify_df.head())
 
-sorted_data = spotify_df.drop(['Unnamed: 0', ])
+sorted_data = spotify_df.drop(['Unnamed: 0'], axis=1)
 sorted_data.drop_duplicates(subset=['track_id'], inplace=True)
+
+
+spotify_df['artists'] = spotify_df['artists'].apply(str)
+
+
+spotify_df['artists_upd'] = spotify_df['artists'].apply(
+    lambda x: re.findall(r"([^']*)", x))
+
+for i in range(spotify_df.shape[0]):
+    spotify_df['artists_upd'][i] = spotify_df['artists_upd'][i][0].split(
+        ';')
+
+spotify_df = spotify_df.explode('artists_upd')
+spotify_df['artists_song'] = spotify_df.apply(
+    lambda row: row['artists_upd']+str(row['track_name']), axis=1)
+spotify_df.drop_duplicates('artists_song', inplace=True)
+print(spotify_df)
